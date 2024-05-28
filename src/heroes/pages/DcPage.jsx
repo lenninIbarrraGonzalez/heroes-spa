39e6967dc93266cbf136dd2727372135dd89c25d
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-05-28 17:17:49 -0500

    feature/LeerArgumentosPorURL: se hace uso del hook useParams para leer los argumentos y obtener los datos de un heroe en especifico

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -5,7 +5,7 @@ export const DcPage = () => {
     <>
       <h1>DC Comics</h1>
       <hr />
-      <HeroList publisher={"DC Comics"} />
+      <HeroList publisher="DC Comics" />
     </>
   );
 };
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,27 @@
+import { Navigate, useParams } from "react-router";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
+  //obtengo los segmentos de los query  params  //hero/dc-batman
+  //custom  hook usePaarams
+  const { id } = useParams();
+
+  // const params = useParams();
+  // console.log("params", params);
+
+  // const { id, ...rest } = useParams();
+  // console.log({ id }, { rest });
+
+  const hero = getHeroById(id);
+  //información del hero
+  //console.log(hero);
+
+  //si el id no existe retorna undefined, vamos a intervenir antes de generar errores
+  if (!hero) {
+    //lo envio a una pagina que si existe
+    return <Navigate to="/marvel" />;
+  }
+
   return (
     <>
       <h1>Hero Page</h1>
