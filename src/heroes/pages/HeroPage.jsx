c35cd00116e7b16d00b8d92dabd10de77cdd0453
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-05-29 10:06:23 -0500

    feature/EstilosHeroScreen: se hace unos delo useParams para capturar el id del hero + se hace uso del usenavigate para retornar a la pagina anterior

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from "react-router";
+import { Navigate, useNavigate, useParams } from "react-router";
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
@@ -16,6 +16,11 @@ export const HeroPage = () => {
   //información del hero
   //console.log(hero);
 
+  const navigate = useNavigate();
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
+
   //si el id no existe retorna undefined, vamos a intervenir antes de generar errores
   if (!hero) {
     //lo envio a una pagina que si existe
@@ -23,8 +28,33 @@ export const HeroPage = () => {
   }
 
   return (
-    <>
-      <h1>Hero Page</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`/assets/heroes/${hero.id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>alter ego:</b> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>publisher</b> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance</b> {hero.first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Regresar
+        </button>
+      </div>
+    </div>
   );
 };
