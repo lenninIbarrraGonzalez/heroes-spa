6c2a2fc77a0698367bea6bb1acacaeb0fbb01e0c
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-05-30 17:23:00 -0500

    feature/ComponentSearch: se crea el componente para la busqueda, se implementa un helper para el filtrado segun el texto ingresado en el input

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,36 @@
+import { useLocation, useNavigate } from "react-router";
+import queryString from "query-string";
+import { useForm } from "../../hooks";
+import { getHeroesByName } from "../helpers";
 import { HeroCard } from "../components";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+  //console.log(location);
+
+  //instalamos el paquete queryString
+  // const query = queryString.parse(location.search);
+  // console.log(query);
+
+  const { q = "" } = queryString.parse(location.search);
+  //console.log({ q });
+
+  const heroes = getHeroesByName(q);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+    //console.log(searchText);
+    //lo utilizamos para pasar el query param
+    navigate(`?q=${searchText}`);
+  };
+
   return (
     <>
       <h1>Search Page</h1>
@@ -10,13 +40,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className="btn btn-outline-primary mt-1">Search</button>
           </form>
@@ -26,9 +58,11 @@ export const SearchPage = () => {
           <hr />
           <div className="alert alert-primary">Search a hero</div>
           <div className="alert alert-danger">
-            No hero whih <b>ABC</b>
+            No hero whih <b>{`${q}`}</b>
           </div>
-          {/* <HeroCard /> */}
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
