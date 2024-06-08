89643d001b6eae002942c324cc146525addc4050
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-06-08 15:52:29 -0500

    feature/RutasPublicas: se implementan rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,20 @@ import { Route, Routes } from "react-router";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
 import { PrivateRoute } from "../heroes/routes/PrivateRoute";
+import { PublicRoutes } from "../heroes/routes/PublicRoutes";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route
+          path="login"
+          element={
+            <PublicRoutes>
+              <LoginPage />
+            </PublicRoutes>
+          }
+        />
 
         <Route
           path="/*"
